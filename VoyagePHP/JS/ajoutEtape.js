f11a17a497c7befc4e9682c0a7f128828a1d33ae
splllatch <maxime.gastellier@hotmail.fr>
2021-09-05 11:34:55 +0200

    aze

---
+++ VoyagePHP/JS/ajoutEtape.js
@@ -0,0 +1,5 @@
+const transport = document.getElementById("logo");
+const transport = document.querySelectorAll('transport');
+
+
+transport.style.color = 'rgb(236, 144, 24)'
--- VoyagePHP/ajoutEtape.php
+++ VoyagePHP/ajoutEtape.php
@@ -3,6 +3,8 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet"  href="CSS/ajoutEtape.css">
+		<script src="JS/ajoutEtape.js" defer></script>
 	</head>
 	<body>
 
@@ -17,27 +19,48 @@
 
 			 
 
-			<label >Ville départ</label> <input type="text"  name="ville1" >
+			<label >Ville départ</label> <input type="text"  name="ville1">
+
+			<div class="transport">
 			<label>Ville arrivée</label> <input type="text" name="ville2">
+			</div>
+			
+			<div class="transport">
 			<label>Type de transport</label> <input type="text" name="typeTransport">
+			</div>
+			
 			<label>Temps</label> <input type="text" name="duree">
 			<label>Unité (jours ou heures)<br></label> 
+
 			<input type="radio" name="unite" value="jours" />jours<br>
 			<input type="radio" name="unite" value="heures" />heures<br>
 
-
-			<label>Cout du billet</label> <input type="text" name="coutBillet">
+			<div class="transport">
+			<label>Cout du billet</label> <input type="text" name="coutBillet">	
+			</div>
+			
 			<label>Cout journalier du pays</label> <input type="text" name="coutPays">
+
+			<div class="ville">
 			<label>Activitée 1</label> <input type="text" name="listeActivitees1">
 			<label>Activitée 2</label> <input type="text" name="listeActivitees2">
 			<label>Activitée 3</label> <input type="text" name="listeActivitees3">
 			<label>Cout des activitées</label> <input type="text" name="coutActivitees">
+			</div>
+
+			<div class="transport">
 			<label>Visa 1</label> <input type="text" name="listeVisa1">
 			<label>Visa 2</label> <input type="text" name="listeVisa2">
 			<label>Visa 3</label> <input type="text" name="listeVisa3">
 			<label>Cout des visa</label> <input type="text" name="coutVisa">
+			</div>
+
 			<label>Pays</label> <input type="text" name="pays">
+
+			<div class="transport">
 			<label>Détails achat billet</label> <input type="text" name="detailAchat">
+			</div>
+
 			<label>Détails de l'étape</label> <input type="text" name="detailEtape">
 
 			<input type="submit" value="Ajouter" >
